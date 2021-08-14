123f34ba2713203d87a42b026918bf6a98325803
SSV0819 <88583745+SSV0819@users.noreply.github.com>
2021-08-14 15:36:51 +0300

    Update 01_task_list.py

--- Module3/practice/list_and_tuple/01_task_list.py
+++ Module3/practice/list_and_tuple/01_task_list.py
@@ -2,5 +2,9 @@
 # Каждый элемент должен быть выведен с новой строки.
 
 my_list = [24, "Васлий", "выхухоль", 42, -12.5]
-
+i = 0
 # TODO: your code here
+for word in my_list:
+    print (my_list[i])
+    i += 1
+print ("End of cicle")
