59f77b8be103222b89d9ca978caddbe817d30994
SSV0819 <88583745+SSV0819@users.noreply.github.com>
2021-08-14 16:06:09 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,11 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+random_list = [-10, 3, 4, 2, 5, -8]
+i = 0
+sum = 0
+for num in random_list:
+    if num > 0:
+        i += 1
+        sum += num
+print (sum)
