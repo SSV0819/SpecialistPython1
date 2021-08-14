02c3550b0ee9f724425395b476da9133db157914
SSV0819 <88583745+SSV0819@users.noreply.github.com>
2021-08-14 15:53:37 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,10 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+random_list = [-10, 3, 4, 2, -8]
+i = 0
+sum = 0
+for num in random_list:
+    i += 1
+    sum += num
+print (sum)
